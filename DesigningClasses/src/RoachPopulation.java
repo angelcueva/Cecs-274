14a37dea33c76fedc91bcec3981f4bc3e238d6eb
angelcueva <48697685+angelcueva@users.noreply.github.com>
2020-02-12 11:55:09 -0800

    Add files via upload

---
+++ DesigningClasses/src/RoachPopulation.java
@@ -0,0 +1,12 @@
+public class RoachPopulation {
+    public int roach_population;
+    public String name_gang;
+    public RoachPopulation(int initial_pop , String name_gang ){
+        roach_population = initial_pop;
+        this.name_gang = name_gang;
+    }
+    public RoachPopulation() {
+
+    }
+
+}
